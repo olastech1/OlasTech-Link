5cc72b0f86b902a18e8d97057212577d336b36bf
olaniyi iyanu <olastrch@olaniyis-MacBook-Pro.local>
2026-06-02 10:05:52 -0500

    Test admin login for auth

--- api/cron/test.js
+++ api/cron/test.js
@@ -1,9 +1,18 @@
-const { authorizeClient } = require('../../server/omada');
+const { _adminLogin, http } = require('../../server/omada');
+const SITE_NAME = process.env.OMADA_SITE_NAME || 'Default';
+
 module.exports = async function (req, res) {
   try {
-    // try to authorize a dummy client to see if it throws Invalid username or password
-    await authorizeClient({ clientMac: '00-11-22-33-44-55', apMac: 'AA-BB-CC-DD-EE-FF', ssidName: 'olastech', duration: 10 });
-    res.json({ success: true });
+    const { cid, cookie, token } = await _adminLogin();
+
+    const payload = {
+      clientMac: '00-11-22-33-44-55', apMac: 'AA-BB-CC-DD-EE-FF',
+      ssidName: 'olastech', radioId: 0, time: 10, authType: 4, site: SITE_NAME,
+    };
+
+    const r = await http.post(`/${cid}/api/v2/hotspot/extPortal/auth`, payload, { headers: { Cookie: cookie, 'Csrf-Token': token } });
+    
+    res.json({ success: true, data: r.data });
   } catch (e) {
     res.json({ error: e.message });
   }
