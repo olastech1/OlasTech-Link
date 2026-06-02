af1475e7365fd099912d24038943bf3cc29d32e4
olaniyi iyanu <olastrch@olaniyis-MacBook-Pro.local>
2026-06-02 09:48:55 -0500

    Update test to check Olastech-pixel

--- api/cron/test.js
+++ api/cron/test.js
@@ -1,16 +1,9 @@
 const db = require('../../server/db');
-const { getClientStats } = require('../../server/omada');
 
 module.exports = async function (req, res) {
   try {
-    const clients = await getClientStats();
-    const activeRes = await db.query(`SELECT id, client_mac, data_used, last_activity_bytes, expires_at FROM sessions ORDER BY started_at DESC LIMIT 5`);
-    
-    res.json({
-      activeDbSessions: activeRes.rows,
-      omadaClientsFound: clients ? clients.length : 0,
-      omadaTopClients: clients ? clients.slice(0, 3) : []
-    });
+    const res1 = await db.query(`SELECT * FROM sessions WHERE client_mac = 'DA-03-AD-C5-62-4E' OR client_mac = 'DA:03:AD:C5:62:4E'`);
+    res.json({ sessionsFound: res1.rows });
   } catch (e) {
     res.json({ error: e.message });
   }
